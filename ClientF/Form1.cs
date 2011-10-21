9f87ff536834184da4741a6eb733809300e85ae1
Thai Nhan <thaiducthanhnhan@gmail.com>
2011-10-21 14:06:32 +0700

    Signed-off-by: Thai Nhan <thaiducthanhnhan@gmail.com>

---
+++ ClientF/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ClientF.nhom;
+
+
+namespace ClientF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Service1Client nhom = new Service1Client();
+            label1.Text = nhom.nhom();
+           
+        }
+    
+    }
+}
---
+++ LogTrace/Service1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace LogTrace
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string nhom()
+        {
+            return "Lai Dai Loc - 093535\nThai Duc Thanh Nhan - 093553\nCung cap thong tin bai hat";
+        }
+
+    }
+}
